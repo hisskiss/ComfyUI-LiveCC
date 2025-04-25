f11b5437d95b59b3a53bf520f91d1af433d72667
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-04-25 16:12:11 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,19 @@
+from .nodes import LoadLiveCCVideo, LoadLiveCCModel, Prompt, LiveCC, SaveLiveCCText
 
+NODE_CLASS_MAPPINGS = {
+    "LoadLiveCCVideo": LoadLiveCCVideo,
+    "LoadLiveCCModel": LoadLiveCCModel,
+    "Prompt": Prompt,
+    "LiveCC": LiveCC,
+    "SaveLiveCCText": SaveLiveCCText,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadLiveCCVideo": "Load LiveCC Video",
+    "LoadLiveCCModel": "Load LiveCC Model",
+    "Prompt": "Prompt",
+    "LiveCC": "LiveCC",
+    "SaveLiveCCText": "Save LiveCC Text",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
